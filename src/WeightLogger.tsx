40619ad0c6afdcf18253a999eca191b5c6d64e24
Alan Wen <alanjameswen@outlook.com>
2022-11-24 16:49:24 -0800

    WeightLogger

--- src/WeightLogger.tsx
+++ src/WeightLogger.tsx
@@ -1,5 +1,5 @@
-import { useTheme, Dialog, Button } from "react-native-paper"
-import { View, StyleSheet, Dimensions } from "react-native"
+import { useTheme, Dialog, Button, IconButton } from "react-native-paper"
+import { Text, View, StyleSheet, Dimensions } from "react-native"
 import { useState } from "react"
 import NumericInput from 'react-native-numeric-input'
 
@@ -14,55 +14,124 @@ export function WeightLogger() {
       justifyContent: "center"
     },
 
-    inputContainer: {
-      display: "flex",
-      flexDirection: "row",
+    dialog: {
+      backgroundColor: theme.colors.onBackground,
+    },
+
+    dialogContent: {
+      flexDirection: "column",
       alignItems: "center",
       justifyContent: "center"
     },
 
-    input: {
-      flex: 1,
+    dialogActions: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+
+    inputContainer: {
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center"
     },
 
     inputSpacer: {
       flex: 1 / 4
-    }
+    },
+
+    inputLabel: {
+      fontSize: 0.05 * Dimensions.get('window').width,
+      color: theme.colors.primary
+    },
   })
+
+  const [weight, setWeight] = useState(0)
+  const [reps, setReps] = useState(0)
 
   return (
     <View style={styles.container}>
-      <Dialog visible={true} onDismiss={() => { }}>
-        <Dialog.Title>Log</Dialog.Title>
-        <Dialog.Content>
+      <Dialog style={styles.dialog} visible={true} onDismiss={() => { }}>
+        <Dialog.Content style={styles.dialogContent}>
           <View style={styles.inputContainer}>
-            <NumericInput
-              totalWidth={Dimensions.get('window').width / 3}
-              type="up-down"
-              rounded
-              step={5}
-              minValue={0}
-              textColor={theme.colors.primary}
-              onChange={value => console.log(value)}
-              />
-            <View style={styles.inputSpacer} />
-            <NumericInput
-              totalWidth={Dimensions.get('window').width / 3}
-              type="up-down"
-              rounded
-              step={1}
-              minValue={0}
-              textColor={theme.colors.primary}
-              onChange={value => console.log(value)}
-            />
+            <Text style={styles.inputLabel}>Weight</Text>
+            <NumberInput value={weight} setValue={setWeight} step={5} min={0} max={1000} />
           </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputLabel}>Reps</Text>
+            <NumberInput value={reps} setValue={setReps} step={1} min={0} max={100} />
+          </View>
+          <IconButton
+            size={Dimensions.get('window').width / 8}
+            iconColor={theme.colors.primary}
+            icon="check-circle"
+          />
         </Dialog.Content>
-        <Dialog.Actions>
-          <Button mode="outlined" onPress={() => { }}>
-            Log
-          </Button>
-        </Dialog.Actions>
       </Dialog>
+    </View>
+  )
+}
+
+interface NumberInputProps {
+  value: number
+  setValue: (v: number) => void
+  min?: number
+  max?: number
+  step: number
+}
+
+function NumberInput(props: NumberInputProps) {
+  const theme = useTheme()
+
+  const styles = StyleSheet.create({
+    container: {
+      // borderWidth: 1,
+      flexDirection: "row",
+      justifyContent: "center",
+      alignContent: "center",
+    },
+
+    valueContainer: {
+      // borderWidth: 1,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+
+    value: {
+      textAlign: "center",
+      color: theme.colors.primary,
+      fontSize: 0.1 * Dimensions.get('window').width,
+      width: Dimensions.get('window').width / 4
+    },
+
+    buttonContainer: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+    },
+  })
+
+  const min = props.min ?? -Infinity
+  const max = props.max ?? Infinity
+
+  return (
+    <View style={styles.container}>
+      <IconButton
+        size={Dimensions.get('window').width / 8}
+        iconColor={theme.colors.primary}
+        icon="arrow-left-drop-circle"
+        onPress={() => props.value > min ? props.setValue(props.value - props.step) : {}}
+        />
+      <View style={styles.valueContainer}>
+        <Text style={styles.value}>{props.value}</Text>
+      </View>
+      <IconButton
+        size={Dimensions.get('window').width / 8}
+        iconColor={theme.colors.primary}
+        icon="arrow-right-drop-circle"
+        onPress={() => props.value < max ? props.setValue(props.value + props.step) : {}}
+      />
     </View>
   )
 }
